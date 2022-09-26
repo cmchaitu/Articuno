3d280d20add4f15839bdfbaab66a88ed6120478e
Chaitanya Mudaddala <cmchaitu@gmail.com>
2022-09-26 19:44:17 +0530

    Add project files.

---
+++ API/Data/StoreDBContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreDBContext : DbContext
+    {
+        public StoreDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
+
---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          var host =  CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+
+        }
+        public static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())  //IServiceScope Used for Scoped Services
+            {
+                var Services = scope.ServiceProvider;     //Resolve Dependency
+                try
+                {
+                    var context = Services.GetRequiredService<StoreDBContext>();
+                    DataInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = Services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error Occured Creating in DB");
+                    throw;
+                }
+            }
+        }
+    
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
